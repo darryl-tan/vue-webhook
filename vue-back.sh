17bac5b231b2af7701a408451d92a263ef743648
limoxuan <55473203@qq.com>
2024-11-25 16:54:16 +0800

    11
    111

--- vue-back.sh
+++ vue-back.sh
@@ -5,7 +5,7 @@ echo "清理代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "开始构建镜像"
 docker build -t vue-back:1.0 .
 echo "删除旧容器"
--- vue-front.sh
+++ vue-front.sh
@@ -5,7 +5,7 @@ echo "清理代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
